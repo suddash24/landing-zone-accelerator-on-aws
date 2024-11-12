a6565b32838e102dd762d6f3c5b10648fe0a4650
Sudeshna Dash <suddash@amazon.de>
2024-11-12 13:49:25 +0100

    fix: pipeline and update tests

--- source/packages/@aws-accelerator/accelerator/lib/stacks/logging-stack.ts
+++ source/packages/@aws-accelerator/accelerator/lib/stacks/logging-stack.ts
@@ -1040,7 +1040,7 @@ export class LoggingStack extends AcceleratorStack {
         this.acceleratorResourceNames.parameters.firehoseRecordsProcessorFunctionName,
       logsKmsKey: this.cloudwatchKey,
       logsRetentionInDaysValue: this.props.globalConfig.cloudwatchLogRetentionInDays.toString(),
-      firehoseLogExtension: this.props.globalConfig.logging.cloudwatchLogs?.firehose?.fileExtension?.toString(),
+      firehoseLogExtension: this.props.globalConfig.logging.cloudwatchLogs?.firehose?.fileExtension,
     });
 
     if (this.centralLogsBucket) {
